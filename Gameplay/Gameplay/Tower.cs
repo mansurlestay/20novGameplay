88906702fb7b0342c6e0604b50f3c0ad1c000951
LestayM <13lestaym@student.smchull.org>
2025-12-04 10:45:53 +0000

    Implemented a calculation that returns whether the given point is inside the strike range of the tower or not.

--- Gameplay/Gameplay/Tower.cs
+++ Gameplay/Gameplay/Tower.cs
@@ -14,6 +14,8 @@ public class Tower
     private Texture2D _tex;
     private double strikeRange = 2.75;
     private int rangeThickness = 4;
+    private double radius;
+    private Vector2 circleCenter;
     private Rectangle[] pointRecArr = new Rectangle[360];
 
     public Tower(Texture2D tex, Rectangle rectangle)
@@ -25,14 +27,22 @@ public class Tower
 
     public void VisualStrikeRadius()
     {
-        double radius = strikeRange * _rectangle.Width;
-        Vector2 circleCenter = new Vector2(_rectangle.X + (_rectangle.Width / 2), _rectangle.Y + (_rectangle.Height / 2));
+        radius = strikeRange * _rectangle.Width;
+        circleCenter = new Vector2(_rectangle.X + (_rectangle.Width / 2), _rectangle.Y + (_rectangle.Height / 2));
         for(int i=0; i < pointRecArr.GetLength(0); i++)
         {
             double angle = ((2 * Math.PI) / pointRecArr.GetLength(0)) * i;
             Vector2 pointPos = new Vector2((int)(radius * Math.Sin(angle) + circleCenter.X), (int)(radius * Math.Cos(angle)) + circleCenter.Y);
             pointRecArr[i] = new Rectangle((int)pointPos.X, (int)pointPos.Y, rangeThickness, rangeThickness);
         }
+    }
+
+    public bool isPointInCircle(int pointX, int pointY)
+    {
+        double rSquared = radius*radius;
+        double calculation = (Math.Pow(pointX - circleCenter.X,2) + Math.Pow(pointY - circleCenter.Y, 2));
+        
+        return calculation <= rSquared;
     }
 
     public void DrawRadius(SpriteBatch sBatch)
